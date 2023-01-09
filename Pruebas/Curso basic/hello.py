83b6883c8263aeef040d042258d9abb627608e83
Javiocu <javiocu@protonmail.com>
2023-01-09 13:25:24 +0100

    Añadidos algunas líneas de texto

--- Pruebas/Curso basic/hello.py
+++ Pruebas/Curso basic/hello.py
@@ -1,6 +1,7 @@
 # Esto es un comentario: Hola mundo
 # Nuestro Hola mundo en Phyton
 print("Hola Python")
+print("Hola Python")
 
 """
 Este es un
@@ -8,4 +9,11 @@ comentario
 en varias líneas
 """
 
+print("----------")
+
 # Tenemos diferentes TIPOS DE DATOS
+# Enteros o integer:
+print(type(10))
+
+# Reales o "float":
+
