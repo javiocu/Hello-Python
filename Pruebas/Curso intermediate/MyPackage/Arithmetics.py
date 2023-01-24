60557e35c4d7a1c6818f070383597326de82bc45
Javiocu <javiocu@protonmail.com>
2023-01-24 19:21:36 +0100

    new file:   Pruebas/Curso intermediate/MyPackage/Arithmetics.py 	new file:   Pruebas/Curso intermediate/MyPackage/___init___.py

---
+++ Pruebas/Curso intermediate/MyPackage/Arithmetics.py
@@ -0,0 +1,21 @@
+### Arithmetics ###
+
+
+def sumando ():
+    primero = input("Por favor introduce el primer número a sumar: ")
+    while True:
+        try:
+            primero = float(primero)
+            break
+        except ValueError:
+            primero = input("Opps, eso no es un número, por favor, introduce un primer número: ")
+    segundo = input("Por favor introduce un segundo número: ")
+    while True:
+        try:
+            segundo = float(segundo)
+            break
+        except ValueError:
+            segundo = input("Eso no es un número, por favor introduce un número: ")
+    suma = primero + segundo
+    final = "La suma de ambos número es: " + str(int(suma))  # Recordar que para que devuelva el valor de return hay que printear
+    return(final)
