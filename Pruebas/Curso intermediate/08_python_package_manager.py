76ac783d85e938c92efff92b27068ddabda7eba7
Javiocu <javiocu@protonmail.com>
2023-01-24 19:21:20 +0100

    modified:   Pruebas/Curso intermediate/08_python_package_manager.py 	Pruebas/Curso intermediate/MyPackage/

--- Pruebas/Curso intermediate/08_python_package_manager.py
+++ Pruebas/Curso intermediate/08_python_package_manager.py
@@ -2,3 +2,31 @@
 
 ## Pip https://pypi.org/
 
+import pip
+
+
+import numpy as np
+
+
+
+numpy_array = np.array([23,34,54,12,6,789,87,45,23,1222,545])
+array_normal = [23,34,54,12,6,789,87,45,23,1222,545]
+
+print(type(numpy_array))
+print(numpy_array + 112)
+print(type(array_normal))
+# print(array_normal +112)      # Con listas no nos deja hacer operaciones, peor con un numpy array sí nos deja operar directamente con esa lista
+
+
+# pip list       # Nos enseña un listado de los módulos instalados en python, en el intérprete
+# pip show pandas # Nos da información sobre ese paquete en concreto
+
+
+import requests # Nos sirve para hacer peticiones a un API
+
+
+
+# Arithmetics Package
+
+import 
+
